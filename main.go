3726dcefc228f16d186612d5a31fd0fa3102bee2
umisama <umisama@fe2o3.jp>
2015-10-11 11:59:37 +0900

    Add http

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/umisama/procon26/plan"
 	"io/ioutil"
 	"math/rand"
+	"net/http"
+	"net/url"
 	"os"
 	"runtime"
 	"runtime/pprof"
@@ -16,9 +18,12 @@ import (
 const PPROF_DIR = "pprof"
 
 var (
-	flagFilePath    = flag.String("f", "", "file name")
-	flagTimeLimit   = flag.Int("t", 180, "time(sec)")
-	flagRepeatCount = flag.Int("n", 3, "max repeat count")
+	flagFilePath      = flag.String("f", "", "file name")
+	flagServerAddress = flag.String("i", "", "server address")
+	flagTeamToken     = flag.String("t", "", "team token")
+	flagQuestNo       = flag.Int("q", -1, "quest number")
+	flagTimeLimit     = flag.Int("s", 180, "time(sec)")
+	flagRepeatCount   = flag.Int("n", 3, "max repeat count")
 )
 
 func main() {
@@ -35,21 +40,60 @@ func main() {
 	pprof.StartCPUProfile(pproff)
 	defer pprof.StopCPUProfile()
 
-	lines, err := getLinesFromFile(*flagFilePath)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	game, err := NewGameMemo(lines, time.Duration(*flagTimeLimit)*time.Second, *flagRepeatCount)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	plan := game.Run()
-	err = outputFile("output.txt", plan)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
+	if *flagFilePath != "" {
+		lines, err := getLinesFromFile(*flagFilePath)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		game, err := NewGameMemo(lines, time.Duration(*flagTimeLimit)*time.Second, *flagRepeatCount)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		plan := game.Run()
+		err = outputFile("output.txt", plan)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	} else if *flagServerAddress != "" && *flagTeamToken != "" && *flagQuestNo != -1 {
+		resp, err := http.Get(fmt.Sprintf("http://%s/quest%d.txt?token=%s", *flagServerAddress, *flagQuestNo, *flagTeamToken))
+		println(fmt.Sprintf("http://%s/quest%d.txt?token=%s", *flagServerAddress, *flagQuestNo, *flagTeamToken))
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		if resp.StatusCode != http.StatusOK {
+			fmt.Println(resp.StatusCode)
+			return
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		lines := strings.Split(string(body), "\r\n")
+		game, err := NewGameMemo(lines, time.Duration(*flagTimeLimit)*time.Second, *flagRepeatCount)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		plan := game.Run()
+		resp, err = http.PostForm(fmt.Sprintf("http://%s/answer", *flagServerAddress), url.Values{
+			"token":  []string{*flagTeamToken},
+			"answer": []string{plan.String()},
+		})
+		if resp.StatusCode != http.StatusOK {
+			fmt.Println("send failed")
+		}
+		err = outputFile("output.txt", plan)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	} else {
+		flag.Usage()
 	}
 	return
 }
